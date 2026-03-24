f5f23c83c08a3ba86741adb17a70c85598102dcb
NileshKonkankar <konkankarnilesh@gmail.com>
2026-03-24 12:20:39 +0530

    feat: implement file upload middleware and PDF text extraction service.

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -1,15 +1,21 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+// Create folder if not exists
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-
- destination: function(req,file,cb){
-  cb(null,"uploads/");
- },
-
- filename: function(req,file,cb){
-  cb(null, Date.now() + "-" + file.originalname);
- }
-
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
 });
 
 const upload = multer({ storage });
--- Backend/services/pdfService.js
+++ Backend/services/pdfService.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const pdf = require("pdf-parse");
+const path = require("path");
 
 async function extractText(filePath) {
 
- const dataBuffer = fs.readFileSync(filePath);
+    const dataBuffer = fs.readFileSync(path.resolve(filePath));
 
- const data = await pdf(dataBuffer);
+    const data = await pdf(dataBuffer);
 
- return data.text;
+    return data.text;
 
 }
 
