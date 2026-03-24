749067d116492bed674ed77a231dad0829b25062
NileshKonkankar <konkankarnilesh@gmail.com>
2026-03-24 17:23:45 +0530

    feat: add PDF text extraction service using pdfParse.

--- Backend/services/pdfService.js
+++ Backend/services/pdfService.js
@@ -1,9 +1,14 @@
-const pdfParse = require("pdf-parse/lib/pdf-parse.js");
-
 async function extractText(buffer) {
     try {
+
+        if (!pdfParse) {
+            throw new Error("pdfParse not loaded properly");
+        }
+
         const data = await pdfParse(buffer);
+
         return data.text;
+
     } catch (error) {
         console.error("PDF PARSE ERROR:", error.message);
         throw new Error("Failed to parse PDF");
